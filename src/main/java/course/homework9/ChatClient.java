10e64da2582f227faa127f54b8e58ebcea852ed8
Mondo <mondogao@gmail.com>
2017-10-19 21:58:12 +0800

    feat(homework9): 完成 ChatClient 基本逻辑

--- src/main/java/course/homework9/ChatClient.java
+++ src/main/java/course/homework9/ChatClient.java
@@ -1,4 +1,57 @@
 package course.homework9;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
 public class ChatClient {
+    public static final String ip = "127.0.0.1";
+
+    private Socket s;
+    private int port;
+    private PrintWriter out;
+    private BufferedReader in;
+    private String room;
+    private boolean stopFlag = false;
+
+    ChatClient(int port, String room) {
+       this.port = port;
+       this.room = room;
+    }
+
+    public ChatClient(int port) {
+        this.port = port;
+    }
+
+    public void start() throws IOException {
+        s = new Socket(ip, port);
+
+        out = new PrintWriter(s.getOutputStream());
+        in = new BufferedReader(new InputStreamReader(s.getInputStream()));
+
+        if (!room.isEmpty()) {
+            out.println(":room " + room);
+        }
+
+        new Thread(() -> {
+            String str;
+            while (!stopFlag) {
+                try {
+                    while (!(str = in.readLine()).isEmpty()) {
+                        System.out.println(str);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+
+        Scanner scan = new Scanner(System.in);
+        while (!stopFlag) {
+            out.println(scan.nextLine());
+        }
+    }
 }
